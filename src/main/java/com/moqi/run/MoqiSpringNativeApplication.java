f4e087150fe3dc7a01b6398e077a059c584f628f
moqi <moqimoqidea@gmail.com>
2021-08-19 14:28:25 +0800

    Init Data

---
+++ src/main/java/com/moqi/run/MoqiSpringNativeApplication.java
@@ -0,0 +1,13 @@
+package com.moqi.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoqiSpringNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoqiSpringNativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moqi/run/MoqiSpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moqi.run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoqiSpringNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
